97c7b93cc42c1967b2472916838565c9c91da527
RRunwal <rachana.runwal@mckesson.com>
2020-02-05 16:14:50 -0500

    RR initial commit with new branch

--- src/main/java/com/galvanize/controllers/MovieController.java
+++ src/main/java/com/galvanize/controllers/MovieController.java
@@ -5,16 +5,37 @@ import com.galvanize.repositories.MovieJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class MovieController {
+
+    public static Logger LOGGER = LoggerFactory.getLogger(MovieJpaRepository.class);
+
     @Autowired
     MovieJpaRepository movieJpaRepository;
 
     @GetMapping("/movies")
     public List<Movie> getAllMovies(){
         return movieJpaRepository.findAll();
+    }
+
+    @GetMapping("/selected-movies")
+    public List<Movie> getSelectedMovies(){
+        return movieJpaRepository.findAllByActorsContainingAndReleasedAfter("Chris Pratt", new Date(1/1/2010));
+    }
+
+    @GetMapping("/selected-movies-2")
+    public Movie getFirstMovie(){
+        return movieJpaRepository.findFirstByActorsContainingAndReleasedAfter("Chris Pratt", new Date(1/1/2010));
+    }
+
+    @GetMapping("/selected-movies-by-title")
+    public List<Movie> getSelectedMovieByTitle(){
+        LOGGER.info("I'm in selected movies method");
+        return movieJpaRepository.findAllByTitleContains("Star");
     }
 }
--- src/main/java/com/galvanize/repositories/MovieJpaRepository.java
+++ src/main/java/com/galvanize/repositories/MovieJpaRepository.java
@@ -5,11 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MovieJpaRepository extends JpaRepository<Movie, Long> {
     Movie findByImdbid(String imdbid);
+
+    //finding all movies (list) for an actor after a specific release date
+    List<Movie> findAllByActorsContainingAndReleasedAfter(String actor, Date released);
+    //finding first movie (single movie) for an actor after a specific release date
+    Movie findFirstByActorsContainingAndReleasedAfter(String actor, Date released);
+    //find using like
+    List<Movie> findAllByTitleContains(String title);
 
     @Query(value = "SELECT * from movies m order by RAND() LIMIT ?1", nativeQuery = true)
     List<Movie> findRandomMovies(int qty);
